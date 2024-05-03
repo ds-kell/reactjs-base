6d6733fd0873c2167540d27f7acdd9210ec060a6
VTI\truyen.nguyentuan <truyen.nguyentuan@vti.com.vn>
2024-05-03 09:31:18 +0700

    update ..

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { useRef } from "react";
 import { useNavigate } from "react-router-dom";
 
 export default function Home() {
   const navigate = useNavigate();
+  const messageInputRef = useRef(null);
+  const responseDivRef = useRef(null);
+  const socket = useRef(new WebSocket("ws://localhost:8088/websocket"));
+
+  socket.current.onopen = function(event) {
+    console.log("Connected to WebSocket server");
+  };
+
+  socket.current.onmessage = function(event) {
+    responseDivRef.current.innerHTML += "<p>" + event.data + "</p>";
+  };
 
   const logout = () => {
     localStorage.clear("token");
@@ -10,15 +21,27 @@ export default function Home() {
     localStorage.clear("role");
     navigate("/login");
   };
+
+  const sendMessage = () => {
+    const message = messageInputRef.current.value;
+    socket.current.send(message);
+    messageInputRef.current.value = "";
+  };
+
   return (
     <div>
       <center>
         Hello
-        <br></br>
+        <br />
         <button onClick={logout} htmlType="submit">
           Logout
         </button>
       </center>
+      <div>
+        <input type="text" ref={messageInputRef} />
+        <button onClick={sendMessage}>Send Message</button>
+        <div ref={responseDivRef}></div>
+      </div>
     </div>
   );
 }
